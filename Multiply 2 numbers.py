4aa71afd3416d392cd167cee4c23dabe9d1eda2b
Prem Kaur <95331860+premkaur949@users.noreply.github.com>
2024-03-05 20:43:57 +0530

    Multiply 2 numbers.py

---
+++ Multiply 2 numbers.py
@@ -0,0 +1,15 @@
+"""
+Program: multiply two numbers
+"""
+
+def main():
+    print("This program multiplies two numbers.")
+    num1 = input("Enter first number: ")
+    num1 = int(num1)
+    num2 = input("Enter second number: ")
+    num2 = int(num2)
+    mul = num1*num2
+    print(mul)
+
+if __name__ == '__main__':
+    main()
